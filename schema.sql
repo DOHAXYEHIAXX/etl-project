614a736736d77e99b3e61909d339e4a18ccca1c0
Doha Yehia <dohayehiaramadan@gmail.com>
2026-09-23 02:48:38 +0200

    Create Sales table in schema.sql

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Sales (
+    order_id INT,
+    customer VARCHAR(100),
+    product VARCHAR(100),
+    quantity INT,
+    unit_price DECIMAL(10, 2),
+    total_amount DECIMAL(10, 2)
+);
